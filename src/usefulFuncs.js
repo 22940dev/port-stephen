2a069f6bbcfd6e3837cdacb4f15b983519c5379d
scribblerp9 <szappia86@gmail.com>
2021-06-29 13:05:20 +1000

    Formatting changes and added build date

--- src/usefulFuncs.js
+++ src/usefulFuncs.js
@@ -1,15 +1,36 @@
 function getObjectByPropertyValue(array, propertyName, propertyValue) {
-    return array.find(o => o[propertyName] == propertyValue)
+    return array.find(o => o[propertyName] == propertyValue);
 }
 
 function getIndexByPropertyValue(array, propertyName, propertyValue) {
-    return array.findIndex(o => o[propertyName] == propertyValue)
+    return array.findIndex(o => o[propertyName] == propertyValue);
 }
 
 String.prototype.toProperCase = function () {
-    return this.replace(/\w\S*/g, function (txt) { return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase(); });
+    return this.replace(/\w\S*/g, function (txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
 };
 
 function humanReadableProxy(data) {
-    return JSON.parse(JSON.stringify(data))
+    return JSON.parse(JSON.stringify(data));
+}
+
+function prettyDate(dateObj) {
+    if (typeof dateObj == 'string') {
+        dateObj = new Date(dateObj);
+    }
+    if (typeof dateObj == 'object') {
+        return (
+            dateObj.toDateString() +
+            ' ' +
+            dateObj.getHours() +
+            ':' +
+            ('0' + dateObj.getMinutes()).slice(-2) +
+            ':' +
+            ('0' + dateObj.getSeconds()).slice(-2)
+        );
+    } else {
+        return 'Invalid date object or string';
+    }
 }
