abb03a113a424524b175f3ed4341236cd0633682
scribblerp9 <szappia86@gmail.com>
2021-06-29 16:54:53 +1000

    Groundwork and basic content
    Added colours, fonts, and some basic content with simple transitions

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,13 +8,17 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                myPurple: '#7C3AED',
-                myBlue: '#1E40AF',
-                myDarkBlue: '#1E3A8A',
+                // https://coolors.co/a9cdef-061a40-0353a4-006daa-003559
+                lightest: '#B9D6F2',
+                light: '#A9CDEF',
+                darkBlue: '#061A40',
+                mediumBlue: '#0353A4',
+                regularBlue: '#006DAA',
+                greyBlue: '#003559',
             },
             fontFamily: {
-                handwritten: ['Zeyada', 'cursive'],
-                typewriter: ['Newsreader', 'serif'],
+                heading: ['Patua One', 'sans-serif'],
+                body: ['Poppins', 'sans-serif'],
             },
         },
     },
